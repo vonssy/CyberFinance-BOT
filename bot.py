6b4633984a21913ddabe1be51078470902df5bed
vonssy <kak.reyvaldizakaria@gmail.com>
2024-10-16 21:40:43 +0700

    add upgrade booster

--- bot.py
+++ bot.py
@@ -113,6 +113,41 @@ class CFI:
         else:
             return None
         
+    def boost_info(self, token: str):
+        url = 'https://api.cyberfin.xyz/api/v1/mining/boost/info'
+        self.headers.update({
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json',
+        })
+
+        response = self.session.get(url, headers=self.headers)
+        if response.status_code == 200:
+            result = response.json()
+            if result['code'] == 200:
+                return result['message']
+            else:
+                return None
+        else:
+            return None
+        
+    def upgrade_boost(self, token: str, type: str):
+        url = 'https://api.cyberfin.xyz/api/v1/mining/boost/apply'
+        data = json.dumps({'boostType': type})
+        self.headers.update({
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json',
+        })
+
+        response = self.session.post(url, headers=self.headers, data=data)
+        if response.status_code == 201:
+            result = response.json()
+            if result['code'] == 200:
+                return result['message']
+            else:
+                return None
+        else:
+            return None
+        
     def claim_daily(self, token: str):
         url = 'https://api.cyberfin.xyz/api/v1/mining/claim/daily'
         self.headers.update({
@@ -216,38 +251,112 @@ class CFI:
         else:
             return None
         
-    def process_query(self, query: str):
+    def question(self):
+        while True:
+            hammer_upgrade = input("Upgrade Hammer Level? [y/n] -> ").strip().lower()
+            if hammer_upgrade in ["y", "n"]:
+                hammer_upgrade = hammer_upgrade == "y"
+                break
+            else:
+                print(f"{Fore.RED+Style.BRIGHT}Invalid Input.{Fore.WHITE+Style.BRIGHT} Choose 'y' to upgrade or 'n' to skip.{Style.RESET_ALL}")
+        hammer_count = 0
+        if hammer_upgrade:
+            while True:
+                try:
+                    hammer_count = int(input("How many times? -> "))
+                    if hammer_count > 0:
+                        break
+                    else:
+                        print(f"{Fore.RED+Style.BRIGHT}Please enter a positive number.{Style.RESET_ALL}")
+                except ValueError:
+                    print(f"{Fore.RED+Style.BRIGHT}Invalid input. Enter a number.{Style.RESET_ALL}")
+        
+        while True:
+            egg_upgrade = input("Upgrade Egg Level? [y/n] -> ").strip().lower()
+            if egg_upgrade in ["y", "n"]:
+                egg_upgrade = egg_upgrade == "y"
+                break
+            else:
+                print(f"{Fore.RED+Style.BRIGHT}Invalid Input.{Fore.WHITE+Style.BRIGHT} Choose 'y' to upgrade or 'n' to skip.{Style.RESET_ALL}")
+        egg_count = 0
+        if egg_upgrade:
+            while True:
+                try:
+                    egg_count = int(input("How many times? -> "))
+                    if egg_count > 0:
+                        break
+                    else:
+                        print(f"{Fore.RED+Style.BRIGHT}Please enter a positive number.{Style.RESET_ALL}")
+                except ValueError:
+                    print(f"{Fore.RED+Style.BRIGHT}Invalid input. Enter a number.{Style.RESET_ALL}")
+
+        return hammer_upgrade, hammer_count, egg_upgrade, egg_count
+    
+    def upgrade_process(self, token, upgrade_type, count):
+        for i in range(count):
+            result = self.upgrade_boost(token, upgrade_type)
+            if result:
+                self.log(
+                    f"{Fore.MAGENTA+Style.BRIGHT}[ Upgarde Boost ]{Style.RESET_ALL}"
+                    f"{Fore.WHITE+Style.BRIGHT} [ {upgrade_type.capitalize()} ] {Style.RESET_ALL}"
+                    f"{Fore.GREEN+Style.BRIGHT}[{i + 1}/{count}]{Style.RESET_ALL}"
+                )
+            else:
+                self.log(
+                    f"{Fore.MAGENTA+Style.BRIGHT}[ Upgarde Boost ]{Style.RESET_ALL}"
+                    f"{Fore.WHITE+Style.BRIGHT} [ {upgrade_type.capitalize()} ] {Style.RESET_ALL}"
+                    f"{Fore.RED+Style.BRIGHT}[{i + 1}/{count}]{Style.RESET_ALL}"
+                )
+            time.sleep(2)
+        
+    def process_query(self, query: str, hammer_upgrade, hammer_count, egg_upgrade, egg_count):
 
         account = self.load_data(query)
         if account:
             self.log(
-                    f"{Fore.MAGENTA+Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                    f"{Fore.WHITE+Style.BRIGHT} {account} {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
-                )
+                f"{Fore.MAGENTA+Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                f"{Fore.WHITE+Style.BRIGHT} {account} {Style.RESET_ALL}"
+                f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
+            )
+            time.sleep(1)
             
         token = self.access_token(query)
+        time.sleep(1)
         if token:
             
             data = self.game_data(token)
             if data:
-                self.log(
-                    f"{Fore.MAGENTA+Style.BRIGHT}[ Balance{Style.RESET_ALL}"
-                    f"{Fore.WHITE+Style.BRIGHT} {data['userData']['balance']} $xCFI {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA+Style.BRIGHT}] [ Streak{Style.RESET_ALL}"
-                    f"{Fore.WHITE+Style.BRIGHT} {data['dailyRewardsData']['currentDay']} {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA+Style.BRIGHT}Day ]{Style.RESET_ALL}                       "
-                )
+                boost = self.boost_info(token)
+                if boost:
+                    self.log(
+                        f"{Fore.MAGENTA+Style.BRIGHT}[ Balance{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} {data['userData']['balance']} $xCFI {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA+Style.BRIGHT}[ Hammer{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} Level {boost['hammerLevel']} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA+Style.BRIGHT}] [ Timer{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} Level {boost['eggLevel']} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}                       "
+                    )
+                time.sleep(1)
+
+                if hammer_upgrade:
+                    self.upgrade_process(token, "HAMMER", hammer_count)
+
+                if egg_upgrade:
+                    self.upgrade_process(token, "EGG", egg_count)
 
                 if not data['dailyRewardsData']['isClaimed']:
                     claim_daily = self.claim_daily(token)
+                    time.sleep(1)
                     if claim_daily:
                         self.log(
                             f"{Fore.MAGENTA+Style.BRIGHT}[ Daily Login{Style.RESET_ALL}"
                             f"{Fore.GREEN+Style.BRIGHT} Claimed {Style.RESET_ALL}"
                             f"{Fore.MAGENTA+Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
                             f"{Fore.WHITE+Style.BRIGHT} {claim_daily['reward']} $xCFI {Style.RESET_ALL}"
-                            f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
+                            f"{Fore.MAGENTA+Style.BRIGHT}] [ Streak{Style.RESET_ALL}"
+                            f"{Fore.WHITE+Style.BRIGHT} {data['dailyRewardsData']['currentDay']} {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA+Style.BRIGHT}Day ]{Style.RESET_ALL}                       "
                         )
                     else:
                         self.log(
@@ -259,8 +368,11 @@ class CFI:
                     self.log(
                         f"{Fore.MAGENTA+Style.BRIGHT}[ Daily Login{Style.RESET_ALL}"
                         f"{Fore.YELLOW+Style.BRIGHT} Already Claimed Today {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
+                        f"{Fore.MAGENTA+Style.BRIGHT}] [ Streak{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} {data['dailyRewardsData']['currentDay']} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA+Style.BRIGHT}Day ]{Style.RESET_ALL}                       "
                     )
+                time.sleep(1)
 
                 crack_time = data['miningData']['crackTime']
                 crack_date = datetime.fromtimestamp(crack_time).astimezone(wib).strftime('%d-%m-%Y %H:%M:%S')
@@ -290,6 +402,7 @@ class CFI:
                         f"{Fore.WHITE+Style.BRIGHT} {crack_date} WIB {Style.RESET_ALL}"
                         f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
                     )
+                time.sleep(1)
 
             else:
                 self.log(
@@ -297,6 +410,7 @@ class CFI:
                     f"{Fore.RED+Style.BRIGHT} {account} {Style.RESET_ALL}"
                     f"{Fore.MAGENTA+Style.BRIGHT}Failed To Load Game Data ]{Style.RESET_ALL}"
                 )
+            time.sleep(1)
 
             ads = self.ads_count(token)
             if ads:
@@ -320,12 +434,14 @@ class CFI:
                                 f"{Fore.RED+Style.BRIGHT}Failed{Style.RESET_ALL}"
                                 f"{Fore.MAGENTA+Style.BRIGHT} ]{Style.RESET_ALL}"
                             )
+                        time.sleep(1)
 
                         count -= 1
                 else:
                     self.log(f"{Fore.YELLOW+Style.BRIGHT}[ Oh Sorry, Ads have Reached Their Limit ]{Style.RESET_ALL}")
             else:
                 self.log(f"{Fore.YELLOW+Style.BRIGHT}[ Ads Not Found ]{Style.RESET_ALL}")
+            time.sleep(1)
 
             tasks = self.game_tasks(token)
             if tasks:
@@ -350,17 +466,21 @@ class CFI:
                                 f"{Fore.RED+Style.BRIGHT}is Not Completed{Style.RESET_ALL}"
                                 f"{Fore.MAGENTA+Style.BRIGHT} ]{Style.RESET_ALL}"
                             )
+                        time.sleep(1)
                             
             else:
                 self.log(f"{Fore.YELLOW+Style.BRIGHT}[ Task Not Found ]{Style.RESET_ALL}")
+            time.sleep(1)
 
         else:
             self.log(f"{Fore.RED+Style.BRIGHT}[ Query Mati ]{Style.RESET_ALL}")
 
     def main(self):
         try:
-            with open('query.txt', 'r') as file:
+            with open('data.txt', 'r') as file:
                 queries = [line.strip() for line in file if line.strip()]
+
+            hammer_upgrade, hammer_count, egg_upgrade, egg_count = self.question()
 
             while True:
                 self.clear_terminal()
@@ -374,7 +494,7 @@ class CFI:
                 for query in queries:
                     query = query.strip()
                     if query:
-                        self.process_query(query)
+                        self.process_query(query, hammer_upgrade, hammer_count, egg_upgrade, egg_count)
                         self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
                         
                 seconds = 1800
